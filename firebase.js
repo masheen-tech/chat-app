ef51e32701b338bd8c1cff8d9c44abd528443e9b
manishsundriyal <msundriyal27@gmail.com>
2022-04-08 16:20:49 +0530

    add env variables

--- firebase.js
+++ firebase.js
@@ -2,16 +2,17 @@ import * as firebase from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
 import { getAuth } from "firebase/auth";
 
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, DATABASE_URL, PROJECT_ID, STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, MEASUREMENT_ID } from '@env';
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 
